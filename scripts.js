7fd8f47f592d4007d3dd67bba776312317691d72
Mondot <valentintintin230704@gmail.com>
2025-07-24 09:49:09 +0200

    Java script

---
+++ scripts.js
@@ -0,0 +1,31 @@
+nom = prompt("Saisissez votre nom");
+prénom = prompt("Saisissez votre prénom");
+alert(prénom);
+alert(nom);
+console.log(nom, prénom);
+
+// je déclare saisir le premier nombre pour calculer le produit
+let z = prompt("Saisie un premier nombre :");
+// Saisie du deuxième nombre
+let b = prompt("Saisie un deuxième nombre :");
+
+//
+let R; // Variable du Résultat//
+R = parseInt(z) + parseInt(b); // Résultat de la valeur de a plus la valeur de b//
+alert("le resultat est de : " + R + ""); // Afiche dans un message d'alerte le résultat
+
+let C = prompt("Saisie un chiffre en °C :");
+let F = (C * 9) / 5 + 32;
+
+alert("le resultat est de : " + F + "°F");
+
+let T = prompt("Saisie la température en °C");
+T = parseInt(R) + F;
+alert("le resultat est de : " + T + " °F en Température");
+
+let chaine = "Ceci est une chaîne de caractères :";
+a = 100;
+b = décrémentation;
+c = 1.0+ a;
+let nouvelechaine = chaine + a;
+alert(nouvelechaine);
